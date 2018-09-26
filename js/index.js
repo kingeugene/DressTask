198812748a8d4382753b8f737e960e53ebd02922
andrew <andrey@innstant.com>
2018-09-26 22:47:46 +0300

    slider added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,7 @@
-$('.carousel[data-type="multi"] .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-    	next = $(this).siblings(':first');
-  	}
-    
-    next.children(':first-child').clone().appendTo($(this));
-  }
+$(document).ready(function (){
+  $('#myCarousel').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    });
 });
